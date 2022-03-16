18fdcddc76c68d7d3864ca55a5652b6c49b3198f
RT <46906497+rtgamingwdt@users.noreply.github.com>
2022-03-16 11:24:23 -0400

    Create Command.js

---
+++ src/base/Command.js
@@ -0,0 +1,12 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+
+module.exports = class Command {
+
+    constructor(options = new SlashCommandBuilder()) {
+        this.get = options;
+    }
+
+    async execute(client, interaction) {
+        console.error(`Missing "execute" method in command: ${this.get.name}`);
+    }
+}
